f73f496713bab74bea1d4ec7b740553041822eea
Lucas Yang <lucas@fionas-macbook-pro.local>
2017-06-05 16:07:57 -0400

    Initial Commit

---
+++ DistanceApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DistanceApp
+//
+//  Created by Lucas Yang on 2017-06-05.
+//  Copyright © 2017 Lucas Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DistanceApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DistanceApp
+//
+//  Created by Lucas Yang on 2017-06-05.
+//  Copyright © 2017 Lucas Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
